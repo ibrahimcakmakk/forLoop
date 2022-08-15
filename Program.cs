4de97d5c962b549ff22f2e60829802700df2325b
ibrahimcakmakk <ibrahim.cakmak@stu.fsm.edu.tr>
2022-08-15 20:32:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args){
+
+        Console.WriteLine("Lütfen bir sayı giriniz");
+        int sayac = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i%2==1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        int tekToplam = 0;
+        int ciftToplam = 0;
+        for (int i = 1; i <= 1000; i++){
+            if (i%2== 1)
+            {
+                tekToplam += i;
+            }else{
+                ciftToplam += i;
+            }
+            Console.WriteLine("tek toplam : " + tekToplam);
+            Console.WriteLine("cift toplam : " + ciftToplam);
+
+
+        }
+       for (int i = 1; i < 10; i++)
+       {
+        if (i==4)
+        {
+            break;
+            Console.WriteLine(i);
+        }
+       }
+       for (int i = 1; i < 10; i++)
+       {
+        if (i==4)
+        {
+            continue;
+            Console.WriteLine(i);
+        }
+       }
+    }
+}
